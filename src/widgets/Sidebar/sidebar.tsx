a8d7ef74f177e9d8b32d99f41dc13f05ad281613
Igor Kovalis <igor_kovalis@rambler.ru>
2025-04-04 13:34:07 +0300

    add sticky

--- src/widgets/Sidebar/sidebar.tsx
+++ src/widgets/Sidebar/sidebar.tsx
@@ -1,14 +1,17 @@
-import st from '@/app/page.module.scss'
+import style from '@/app/page.module.scss'
+import st from './Sidebar.module.scss'
 
 const Sidebar = () => {
   return (
     <aside className={st.sidebar}>
-      <h2>SIDEBAR</h2>
-      <form action="" className={st.filter}>
-        <input type="text" placeholder="Поиск" className={st.filterItem} />
-        <input type="number" placeholder="Минимальная цена" className={st.filterItem} />
-        <input type="number" placeholder="Максимальная цена" className={st.filterItem} />
-      </form>
+      <div className={st.sidebarInner}>
+        <h2>SIDEBAR</h2>
+        <form action="" className={style.filter}>
+          <input type="text" placeholder="Поиск" className={style.filterItem} />
+          <input type="number" placeholder="Минимальная цена" className={style.filterItem} />
+          <input type="number" placeholder="Максимальная цена" className={style.filterItem} />
+        </form>
+      </div>
     </aside>
   )
 }
